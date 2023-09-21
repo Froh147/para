3b2e27c7c2af843700d76a0b846b5d10b59e5d34
Froh147 <145351909+Froh147@users.noreply.github.com>
2023-09-21 09:50:14 +0300

    Add files via upload

---
+++ paawdasdsa.py
@@ -0,0 +1,6 @@
+def number(n):
+    if n%2 == 0:
+        return True
+    else:
+        return False
+print(number(int(input('Ввести число:'))))
---
+++ papdpaps2.py
@@ -0,0 +1,5 @@
+def rectangle(width, height):
+    result=width*height
+    return result
+x=rectangle(float(input('Ввести первое число:')), float(input('Ввести второе число:')))
+print ("Результат ", x)
